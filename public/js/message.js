cda467c1c4bc31a23c27f1483f7085a3f9e42d29
Palereed <544989630@qq.com>
2017-06-09 21:40:40 +0800

    Json to message answer with no refresh

--- public/js/message.js
+++ public/js/message.js
@@ -40,10 +40,11 @@ for( var i = 0;i<Answer.length;i++){
 	}
 }
 // 头像选择  
-var ShowImg = document.getElementById('ShowImg'),
-    ImgChoose = document.getElementById('ImgChoose'),
-    WhichImg = ImgChoose.getElementsByTagName('img'),
-    IsChoose = true;
+if (document.getElementById('ShowImg')){
+    var ShowImg = document.getElementById('ShowImg'),
+	    ImgChoose = document.getElementById('ImgChoose'),
+	    WhichImg = ImgChoose.getElementsByTagName('img'),
+	    IsChoose = true;
     ShowImg.onclick = function(){
 	   if(IsChoose){
 		  ImgChoose.style.display = 'block';
@@ -59,5 +60,92 @@ var ShowImg = document.getElementById('ShowImg'),
 		  ShowImg.setAttribute('src',Imgsrc);
 		  ImgChoose.style.display = 'none';
 		  IsChoose = true;
-	}
+	   }
+   }
 }
+$(function(){
+	//留言
+        $('#Leave').find('button').click(function(){
+          $.ajax({
+            type: 'post',
+            url: '/api/visiter/message',
+            data: {
+                  content :$('#Leave').find('[name="content"]').val(),
+              },
+              dataType: 'json',
+              success:function(result){
+                var code = result.code;
+                $('#Leave').find('[name="content"]').val('');
+                switch (code){
+                  case 0:
+                     $('.LeaveBtn').find('i').removeClass().addClass('fa fa-check');
+                     $('.LeaveBtn').find('div').removeClass().addClass('alert alert-success');
+                     window.location.reload();
+                  break;
+                  default:
+                      $('.LeaveBtn').find('i').removeClass().addClass('fa fa-close');
+                      $('.LeaveBtn').find('div').removeClass().addClass('alert alert-danger');
+                  break;
+                }
+                $('.LeaveBtn').find('span').html(result.message);
+              }
+          })
+        })
+        //留言回复
+        $('.AnswerForm').find('button').click(function(){
+            $(this).parents('form').addClass('WarnWhich');
+            $.ajax({
+            type: 'post',
+            url: '/api/message/answer',
+            data: {
+                  content :$(this).parents('form').find('[name="content"]').val(),
+                  messageId :$(this).parents('form').find('[name="messageId"]').val(),
+              },
+            dataType: 'json',
+            success:function(result){
+                var code = result.code;
+                switch (code){
+                  case 0:
+                      $('.WarnWhich').find('.WarningHold').find('i').removeClass().addClass('fa fa-check');
+                      $('.WarnWhich').find('.WarningHold').find('div').removeClass().addClass('alert alert-success');
+                  break;
+                  default:
+                      $('.WarnWhich').find('.WarningHold').find('i').removeClass().addClass('fa fa-close');
+                      $('.WarnWhich').find('.WarningHold').find('div').removeClass().addClass('alert alert-danger');
+                  break;
+                }
+                $('.WarnWhich').find('.WarningHold').find('span').html(result.message);
+                $('.WarnWhich').find('textarea').val('');
+                renderAnswer(result.data);
+                setTimeout(function(){
+                    $('.WarnWhich').find('.WarningHold').find('i').removeClass();
+                    $('.WarnWhich').find('.WarningHold').find('div').removeClass();
+                    $('.WarnWhich').find('.WarningHold').find('span').html('');
+                    $('.WarnWhich').removeClass();
+                 }, 1000);
+             }
+          })
+        })
+        function renderAnswer(answer){
+            var html = $('.WarnWhich').parents('.AllAnswer').children('.AnswerHold').html();
+            var date = new Date(+new Date(answer.answerTime)+8*3600*1000).toISOString().replace(/T/g,' ').replace(/\.[\d]{3}Z/,'');   
+            html += `<div class="AnswerContain">
+                          <div class="ImgContain">
+                             <img src="${answer.visiterImg}" class="LetterImg">
+                          </div>
+                          <div class="LetterContain">
+                             <div class="VisitInfo">
+                               <span class="VisitName"><a href="#">${decodeURI(answer.visiter)}</a></span> 
+                               <span class="TimeContain">回复于 
+                                  <b class="VisitTime">${date}</b>
+                               </span>
+                             </div>
+                             <div class="LetterReal">
+                               <p>${answer.content}</p>
+                             </div>
+                          </div>
+                          <div class="clearfix"></div>
+                      </div>`
+            $('.WarnWhich').parents('.AllAnswer').children('.AnswerHold').html(html)
+     }
+})
